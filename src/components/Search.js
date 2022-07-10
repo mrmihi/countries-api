fbb05245f8acd0b7eb4fbd71bdfb4c75e5bec2ac
Pasindu Dinal Sumanasekara <51123579+mrmihi@users.noreply.github.com>
2022-07-10 21:51:56 +0530

    Revert "Added the frontend 🔝"
    This reverts commit 4b7d2c5c90bfd9b1d89f38be75f6cea71b568fc5.

--- src/components/Search.js
+++
@@ -1,9 +0,0 @@
-const Search = (props) => {
-    return(
-        <div className="search">
-           <input className="input" onChange={props.onChange} placeholder="  Enter Country Name..."/>
-        </div>
-    )
-}
-
-export default Search
